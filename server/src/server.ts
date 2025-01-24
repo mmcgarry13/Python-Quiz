8553ef897459a641d12f2ac51b39f714db490656
mmcgarry13 <mcgarry31@outlook.com>
2025-01-23 20:59:07 -0500

    dummy pull req

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('../client/dist'));
 
 app.use(routes);
-console.log("test3");
+console.log("test4");
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
